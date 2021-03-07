a447b6fab1f31af3c3bef97ef0bd98a07f398850
Ejaz Ali <ejazali4@protonmail.ch>
2021-03-07 18:11:37 +0000

    💄 Color changer on backg

--- js/index.js
+++ js/index.js
@@ -1,3 +1,33 @@
+fetch('/components/backgrounds.json').then((response) => {
+  if (response.status == 200){
+    response.json().then((data)=>{
+      const randomNum = Math.floor(getRandomArbitrary(0, data.length - 1));
+      console.log(randomNum);
+      console.log(data[randomNum]);
+      document.getElementById("background-css").innerText = `
+      .page{
+          background-image: url('/images/unsplash/${data[randomNum].id}.jpeg');
+      }
+      `
+      getImageLightness(`/images/unsplash/${data[randomNum].id}.jpeg`,function(brightness){
+        console.log(brightness);
+        if (brightness > 120){
+          document.getElementById("text-css").innerText = `
+          :root{
+                    --backg-text-color: black;
+                    --backg-text-color-rgb: 0;
+                }
+      `
+        }
+      });
+    })
+  }
+})
+
+const getRandomArbitrary = (min, max) => {
+    return Math.random() * (max - min) + min;
+}
+
 const timetick = () => {
 
     const d = new Date();
@@ -75,4 +105,39 @@ const slideEverything = (arg) =>{
         item.setAttribute("slide","cslidden");
     });
     document.getElementsByClassName("settings-closed")[0].setAttribute("class","settings");
+}
+
+function getImageLightness(imageSrc,callback) {
+    var img = document.createElement("img");
+    img.src = imageSrc;
+    img.style.display = "none";
+    document.body.appendChild(img);
+
+    var colorSum = 0;
+
+    img.onload = function() {
+        // create canvas
+        var canvas = document.createElement("canvas");
+        canvas.width = this.width;
+        canvas.height = this.height;
+
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(this,0,0);
+
+        var imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
+        var data = imageData.data;
+        var r,g,b,avg;
+
+        for(var x = 0, len = data.length; x < len; x+=4) {
+            r = data[x];
+            g = data[x+1];
+            b = data[x+2];
+
+            avg = Math.floor((r+g+b)/3);
+            colorSum += avg;
+        }
+
+        var brightness = Math.floor(colorSum / (this.width*this.height));
+        callback(brightness);
+    }
 }
