5a54d1da95470ca937bbc5231e5bcf0370eefa70
Ejaz <73548455+ejaz4@users.noreply.github.com>
2021-05-11 09:57:39 +0100

    Update time.js

--- js/widgets/time.js
+++ js/widgets/time.js
@@ -1,67 +1,84 @@
 class TimeWidget {
     constructor() {
-        const getOrdinalNum = (number) => {
-        let selector;
-
-        if (number <= 0) {
-            selector = 4;
-        } else if ((number > 3 && number < 21) || number % 10 > 3) {
-            selector = 0;
+        var ls = window.localStorage;
+        var settings = 0;
+        settings = JSON.parse(ls.getItem('timeSettings')); 
+        if (settings == null) {
+            settings = {
+                "widget-position": "top-left",
+                "time-format": "hh:mm",
+                "24-hour": true,
+                "display": true
+            };
+            ls.setItem('timeSettings', JSON.stringify(settings));
+        }
+        if (settings["display"] == false) {
+            return;
         } else {
-            selector = number % 10;
-        }
-        return number + ['th', 'st', 'nd', 'rd', ''][selector];
-        };
+            const getOrdinalNum = (number) => {
+            let selector;
 
-        const timeWidgetWorker = new Date();
+            if (number <= 0) {
+                selector = 4;
+            } else if ((number > 3 && number < 21) || number % 10 > 3) {
+                selector = 0;
+            } else {
+                selector = number % 10;
+            }
+            return number + ['th', 'st', 'nd', 'rd', ''][selector];
+            };
 
-        const nth = getOrdinalNum(timeWidgetWorker.getDate());
-        const months = [
-            'January',
-            'February',
-            'March',
-            'April',
-            'May',
-            'June',
-            'July',
-            'August',
-            'September',
-            'October',
-            'November',
-            'December'
-        ];
-
-        const days = [
-            'Sunday',
-            'Monday',
-            'Tuesday',
-            'Wednesday',
-            'Thursday',
-            'Friday',
-            'Saturday'
-        ];
-
-        const timeWidgetElementFormat = `
-        <time
-        style='color: var(--backg-text-color); font-size: 5rem; font-weight: 600;'>
-            ${timeWidgetWorker.getHours().toString().padStart(2, '0')}:${timeWidgetWorker.getMinutes().toString().padStart(2, '0')}
-        </time>
-        <time
-        style='display: block; color: var(--backg-text-color); font-size: 1.5rem;font-weight: 400; opacity: 0.7;'>
-            ${days[timeWidgetWorker.getDay()]}, ${nth} ${months[timeWidgetWorker.getMonth()]}
-        </time>`;
-
-        const TimeWidgetTick = () => {
             const timeWidgetWorker = new Date();
-            document.getElementById('time@dothq.co').innerHTML = timeWidgetElementFormat;
-        }
 
-        var timeWidgetElement = document.createElement('div');
-        timeWidgetElement.setAttribute('id', 'time@dothq.co');
-        timeWidgetElement.setAttribute('class', 'widget top-left');
-        timeWidgetElement.innerHTML = timeWidgetElementFormat;
-        document.getElementById('widgets-container').appendChild(timeWidgetElement);
-        setInterval(TimeWidgetTick, 995);
+            const nth = getOrdinalNum(timeWidgetWorker.getDate());
+            const months = [
+                'January',
+                'February',
+                'March',
+                'April',
+                'May',
+                'June',
+                'July',
+                'August',
+                'September',
+                'October',
+                'November',
+                'December'
+            ];
+
+            const days = [
+                'Sunday',
+                'Monday',
+                'Tuesday',
+                'Wednesday',
+                'Thursday',
+                'Friday',
+                'Saturday'
+            ];
+
+            const TimeWidgetTick = () => {
+                const timeWidgetWorker = new Date();
+                settings = JSON.parse(ls.getItem('timeSettings')); 
+                const timeFormat = settings["time-format"].replace("hh", timeWidgetWorker.getHours().toString().padStart(2, '0')).replace("mm", timeWidgetWorker.getMinutes().toString().padStart(2, '0')).replace("ss", timeWidgetWorker.getSeconds().toString().padStart(2, '0')).replace("ms", timeWidgetWorker.getMilliseconds().toString().padStart(2, '0'));
+                const timeWidgetElementFormat = `
+                    <time
+                    style='color: var(--backg-text-color); font-size: 5rem; font-weight: 600;'>
+                        ${timeFormat}
+                    </time>
+                    <time
+                    style='display: block; color: var(--backg-text-color); font-size: 1.5rem;font-weight: 400; opacity: 0.7;'>
+                        ${days[timeWidgetWorker.getDay()]}, ${nth} ${months[timeWidgetWorker.getMonth()]}
+                    </time>`;
+                document.getElementById('time@dothq.co').innerHTML = timeWidgetElementFormat;
+            }
+
+            var timeWidgetElement = document.createElement('div');
+            timeWidgetElement.setAttribute('id', 'time@dothq.co');
+            timeWidgetElement.setAttribute('class', `widget ${settings["widget-position"]}`);
+            document.getElementById('widgets-container').appendChild(timeWidgetElement);
+            TimeWidgetTick();
+            setInterval(TimeWidgetTick, 15);
+        }
     }
 }
 
